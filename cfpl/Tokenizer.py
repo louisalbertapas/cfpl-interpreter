b40ac5926060aaf92e62d9103f48e9b01724ed41
Louis Albert Apas <louisalbertapas25@gmail.com>
2020-11-14 11:59:05 +0800

    Create Tokenizer class for basic arithmetic expression

---
+++ cfpl/Tokenizer.py
@@ -0,0 +1,76 @@
+"""
+File:           Tokenizer.py
+Description:    A Tokenizer class breaks down the code into tokens during lexical analysis
+Author:         Louis Albert Apas (louisalbertapas25@gmail.com)
+
+Copyright 2020
+"""
+
+from cfpl import Token
+from constants.reserved_keywords import *
+
+
+class Tokenizer(object):
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_char = self.text[self.pos]
+
+    def error(self):
+        raise Exception('Invalid character')
+
+    def advance(self):
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None  # Indicates end of input
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+    def get_next_token(self):
+        while self.current_char is not None:
+
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.current_char.isdigit():
+                return Token(INT_CONST, self.integer())
+
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
+
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+
+            if self.current_char == '*':
+                self.advance()
+                return Token(MUL, '*')
+
+            if self.current_char == '/':
+                self.advance()
+                return Token(DIV, '/')
+
+            if self.current_char == '(':
+                self.advance()
+                return Token(LEFT_PAREN, '(')
+
+            if self.current_char == ')':
+                self.advance()
+                return Token(RIGHT_PAREN, ')')
+
+            self.error()
+
+        return Token(EOF, None)
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from constants.reserved_keywords import *
+from cfpl.Token import Token
+from cfpl.Tokenizer import Tokenizer
 
-print(RESERVED_KEYWORDS['START'])
-print(Token(STOP, STOP))
+# Sample test for tokenizing basic arithmetic expression
+tokenizer = Tokenizer('4 + 2 * 1 / (1 + 20)')
+while True:
+    token = tokenizer.get_next_token()
+    print(token)
+    if token.__str__() == "Token(EOF, None)":
+        break
