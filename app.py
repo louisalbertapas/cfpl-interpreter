b359f5e772a3e9d6d488f693bbe78f665116c537
Louis Albert Apas <louisalbertapas25@gmail.com>
2020-11-29 16:34:57 +0800

    Invoke interpreter in app.py

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
+# cfpl imports
+from cfpl.Tokenizer import Tokenizer
+from cfpl.Parser import Parser
+from cfpl.Interpreter import Interpreter
+
+# flask imports
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/', methods=['post', 'get'])
 def interpret_code():
-    message = ''
+    code = ''
     output = ''
     if request.method == 'POST':
-        message = request.form.get('code_in')  # access the data inside
-        output = message
+        code = request.form.get('code_in')  # access the data inside
+        tokenizer = Tokenizer(code)
+        parser = Parser(tokenizer)
+        interpreter = Interpreter(parser)
+        try:
+            interpreter.interpret()
+            output = interpreter.output
+        except Exception as e:
+            print(e)
 
-    return render_template('cfpl.html', message=message, output=output)
+    return render_template('cfpl.html', message=code, output=output)
 
 
 if __name__ == '__main__':
